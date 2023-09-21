fbd868527390b5a84915e2f98ea2bc48d60b7ea0
YosifovaAnna <awsmjk@gmail.com>
2023-09-21 14:08:04 +0300

    Update

--- practice_1/task_3.py
+++ practice_1/task_3.py
@@ -5,9 +5,26 @@ BMI) человека. Пользователь должен ввести сво
 используете одну из приведенных ниже формул для определения индекса.
 BMI = вес/рост**2 
 """
-weight = input()
-height = input()
 
+# Ваш код
+print("Эта программа рассчитывает индекс массы тела (body mass index – BMI) человека")
 
-#Ваш кол
+while True:
+    try:
+        print("Введите ниже свои вес (кг) и рост (см)")
+        weight_input = str(input("Вес: "))
+        height_input = str(input("Рост: "))
 
+        weight = float(weight_input.replace(",", "."))
+        height = float(height_input.replace(",", ".")) / 100
+
+        if weight <= 0 or height <= 0:
+            print("Проверьте корректность данных и введите их снова")
+            continue
+        else:
+            bmi = round(weight / pow(height, 2), 1)
+            print(f"Индекс массы тела (BMI) равен {bmi}")
+
+    except Exception as e:
+        print("Проверьте корректность данных и введите их снова")
+        continue
